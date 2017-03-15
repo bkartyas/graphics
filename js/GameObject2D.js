4c879b93b691f1d5c8ffa6005887304952873cb0
bkartyas <bkartyas@gmail.com>
2017-03-15 18:08:50 +0100

    HF2

--- js/GameObject2D.js
+++ js/GameObject2D.js
@@ -7,6 +7,8 @@ var GameObject2D = function(mesh) {
 
   this.modelMatrix = new Mat4();
   this.updateModelTransformation();
+
+  this.dead = false;
 };
 
 GameObject2D.prototype.updateModelTransformation = function(){
@@ -14,6 +16,17 @@ GameObject2D.prototype.updateModelTransformation = function(){
     scale(this.scale).
     rotate(this.orientation).
     translate(this.position);
+
+};
+
+GameObject2D.prototype.collisions = function(objects){
+  for(var i = 0; i < objects.length; i++){
+  	var otherPos = objects[i].object.position;
+  	var distance = Math.sqrt((this.position.x - otherPos.x)*(this.position.x - otherPos.x) + (this.position.y - otherPos.y) * (this.position.y - otherPos.y));
+  	if(distance !== 0 && distance < 0.12){
+  		objects[i].collide(this);
+  	}
+  }
 
 };
 
@@ -29,9 +42,6 @@ GameObject2D.prototype.move = function(camera){};
 
 
 
-
-
-
 var Asteroid = function(mesh, position) {
   this.object = new GameObject2D(mesh);
 
@@ -42,11 +52,104 @@ var Asteroid = function(mesh, position) {
 
   this.object.position = position;
   this.object.scale = new Vec3((1/10)/1.5, (1/5)/1.5, 1);
+  this.jump;
+  this.life = 1;
 };
 
 Asteroid.prototype.move = function(dt){
-  this.object.position.y -= dt/10;
+  if(!!this.jump){
+  	this.object.position.x += this.jump.x;
+  	this.object.position.y += this.jump.y;
+  	this.jump = null;
+  } else {
+  	this.object.position.y -= dt/10;
+  }
 
   if(this.object.position.y < -1.2){ return true; }
   return false;
+};
+
+Asteroid.prototype.collide = function(other){
+	if(!!this.life){
+		this.jump = new Vec3((this.object.position.x - other.position.x)/5,
+							 (this.object.position.y - other.position.y)/5,
+							 0);
+		this.life--;
+	} else {
+  		this.object.dead = true;
+  	}
+};
+
+
+var Rocket = function(mesh, position) {
+  this.object = new GameObject2D(mesh);
+
+  this.object.mesh.material.offsetMatrix.set(1/2, 0.0, 0.0, 1/2,
+	                                         0.0, 1/2, 0.0, 1/2,
+	                                         0.0, 0.0, 0.0, 0.0,
+	                                         0.0, 0.0, 0.0, 1.0);
+
+  this.object.position = position;
+  this.object.orientation = 3.141592;
+  this.object.scale = new Vec3(1/10, 1/5, 0);
+
+  this.cooldown = 1;
+};
+
+Rocket.prototype.shot = function(dt){
+	this.cooldown += dt;
+	if(this.cooldown > 1){
+		this.cooldown = 0;
+		return true;
+	}
+};
+
+Rocket.prototype.moveLeft = function(dt){
+	if(this.object.position.x < -0.9){
+		this.object.position.x = -0.9;
+	} else {
+		this.object.position.x -= dt/7;
+	}
+};
+
+Rocket.prototype.moveRight = function(dt){
+	if(this.object.position.x > 0.9){
+		this.object.position.x = 0.9;
+	} else {
+		this.object.position.x += dt/7;
+	}
+};
+
+Rocket.prototype.collide = function(other){
+	this.object.dead = true;
+};
+
+var Bullet = function(mesh, position) {
+  this.object = new GameObject2D(mesh);
+
+  this.object.mesh.material.offsetMatrix.set(1/2, 0.0, 0.0, 1/2,
+	                                         0.0, 1/2, 0.0, 1/2,
+	                                         0.0, 0.0, 0.0, 0.0,
+	                                         0.0, 0.0, 0.0, 1.0);
+
+  this.object.position = position;
+  this.object.orientation = -1.57;
+  this.object.scale = new Vec3(1/8, 1/16, 0);
+};
+
+Bullet.prototype.move = function(dt){
+	this.object.position.y += dt/5;
+
+	if(this.object.position.y > 1.2){ return true; }
+};
+
+Bullet.prototype.move = function(dt){
+	this.object.position.y += dt/5;
+
+	if(this.object.position.y > 1.2){ return true; }
+};
+
+
+Bullet.prototype.collide = function(other){
+	this.object.dead = true;
 };
--- js/Scene.js
+++ js/Scene.js
@@ -11,26 +11,26 @@ var Scene = function(gl) {
   this.camera = new OrthoCamera();
 
   this.asteroidMaterial = new Material(gl, this.solidProgram);
+  this.rocketMaterial = new Material(gl, this.solidProgram);
   this.bulletMaterial = new Material(gl, this.solidProgram);
+  this.backgroundMaterial = new Material(gl, this.solidProgram);
 
   this.asteroidMaterial.colorTexture.set(new Texture2D(gl, "media/asteroid.png"));
+  this.rocketMaterial.colorTexture.set(new Texture2D(gl, "media/rocket.png"));
   this.bulletMaterial.colorTexture.set(new Texture2D(gl, "media/bullet.png"));
+  this.backgroundMaterial.colorTexture.set(new Texture2D(gl, "media/background.png"));
 
-  this.boomTexture = new Texture2D(gl, "media/boom.png");
-  this.rocketTexture = new Texture2D(gl, "media/rocket.png");
-  this.backgroundTexture = new Texture2D(gl, "media/background.jpg");
+  //this.boomTexture = new Texture2D(gl, "media/boom.png");
+  this.background = new GameObject2D(new Mesh(this.quadGeometry, this.backgroundMaterial), new Vec3(0, 0, 0));
+  this.rocket = new Rocket(new Mesh(this.quadGeometry, this.rocketMaterial), new Vec3(0, -0.85, 0));
 
+  this.objects = [ this.rocket ];
 
+  this.background.mesh.material.offsetMatrix.set(1/2, 0.0, 0.0, 1/2,
+                                                 0.0, 1/4, 0.0, 1/2,
+                                                 0.0, 0.0, 0.0, 0.0,
+                                                 0.0, 0.0, 0.0, 1.0);
 
-  this.bulletMaterial.offsetMatrix.set(1/2, 0.0, 0.0, 1/2,
-                                         0.0, 1/2, 0.0, 1/2,
-                                         0.0, 0.0, 0.0, 0.0,
-                                         0.0, 0.0, 0.0, 1.0);
-
-  this.bullet = new GameObject2D(new Mesh(this.quadGeometry, this.bulletMaterial));
-  this.bullet.scale = new Vec3(1/10, 1/5, 0);
-
-  this.objects = [];
 };
 
 Scene.prototype.update = function(gl, keysPressed) {
@@ -45,23 +45,52 @@ Scene.prototype.update = function(gl, keysPressed) {
     this.objects.push(new Asteroid(new Mesh(this.quadGeometry, this.asteroidMaterial), new Vec3(Math.random() * 2 - 1, 1.2, 0)));
   }
 
+  if(!!this.rocket){
+    if(keysPressed["SPACE"]){
+      if(this.rocket.shot(dt)){
+        this.objects.push(new Bullet(new Mesh(this.quadGeometry, this.bulletMaterial), new Vec3(this.rocket.object.position.x, -0.6, 0)));
+      }
+    }
+
+    if(keysPressed["A"]){
+      this.rocket.moveLeft(dt);
+    }
+
+    if(keysPressed["D"]){
+      this.rocket.moveRight(dt);
+    }
+  }
+
   // clear the screen
   gl.clearColor(164/255, 66/255, 220/255, 1.0);
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  this.background.draw(this.camera);
   /*var a = 1/6 * Math.floor(dt*speed);
   var b = 1/6 * Math.floor(dt*speed/6);*/
 
+  this.rocket.object.updateModelTransformation();
+  this.rocket.object.collisions(this.objects);
+  this.rocket.object.draw(this.camera);
+  var dead = this.rocket.object.dead;
+  if(dead){ this.rocket = null; }
 
-
-  for(let i = 0; i < this.objects.length; i++){
+  for(let i = 1; i < this.objects.length; i++){
     this.objects[i].object.updateModelTransformation();
-    var dead = this.objects[i].move(dt);
-    if(dead){
-      this.objects.splice(i, 1);
-    }
     this.objects[i].object.draw(this.camera);
+
+    this.objects[i].object.collisions(this.objects);
+  }
+
+  for(let i = 1; i < this.objects.length; i++){
+    this.objects[i].move(dt);
+  }
+
+  for(let i = 1; i < this.objects.length; i++){
+    var dead = this.objects[i].object.dead;
+
+    if(dead){ this.objects.splice(i, 1); }
   }
 
 };
